3f4b924ea86e69a28c61352aabee17d66bbb456c
13718972117 <13718972117@163.com>
2018-12-20 15:55:04 +0800

    路由按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter, HashRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import BundleLoader from './bundleLoader';
 
 const makeRouterTree = (routes) => {
