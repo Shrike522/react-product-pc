7a7dd82f4e62c35be669826898e4f063d2887cda
13718972117 <13718972117@163.com>
2018-12-20 15:47:38 +0800

    路由按需加载

---
+++ src/router/bundleLoader.js
@@ -0,0 +1,39 @@
+// bundle.js 用于按需加载
+import React, { Component } from 'react';
+
+export default class Bundle extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+            mod: null
+        };
+    }
+
+    componentWillMount () {
+        this.load(this.props);
+    }
+
+    componentWillReceiveProps (nextProps, nextContext) {
+        if(nextProps.load !== this.props.load){
+            this.load(nextProps);
+        }
+    }
+
+    load (props) {
+        this.setState({
+            mod: null
+        }, () => {
+            props.load().then((mod) => {
+                this.setState({
+                    mod: mod.default ? mod.default : mod
+                })
+            });
+        })
+    }
+
+    render () {
+        const { mod } = this.state;
+        if (!mod) return false;
+        return this.props.children(mod);
+    }
+}
---
+++ src/router/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Route, BrowserRouter, HashRouter, Switch } from 'react-router-dom';
+import BundleLoader from './bundleLoader';
+
+const makeRouterTree = (routes) => {
+    return routes.map((item) => {
+        const { children, component, path } = item;
+        if (children && Array.isArray(children) && children.length>0){
+            item.delete("children");
+            return (
+                <Route key={`RouteChildren-${path}`} {...item}>
+                    { makeRouterTree(children) }
+                </Route>
+            );
+        }
+
+        // 按需加载
+        item.component = (props) => (
+            <BundleLoader load={() => import(`../pages/${component}`)}>
+                {(Container) => <Container {...props} />}
+            </BundleLoader>
+        );
+
+        return (
+            <Route key={`Route-${path}`} {...item} />
+        );
+    });
+};
+
+const routes = [
+    {
+        path: "/",
+        component: "Home",
+        exact: true
+    },
+    {
+        path: "/detail",
+        component: "Detail"
+    },
+    {
+        path: "/list",
+        component: "List"
+    },
+    {
+        path: "/login",
+        component: "Login"
+    },
+    {
+        path: "/user",
+        component: "User"
+    },
+];
+
+const RouterTree = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                {
+                    makeRouterTree(routes)
+                }
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default RouterTree;
